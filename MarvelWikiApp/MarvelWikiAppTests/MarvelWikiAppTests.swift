15c9d971f0fef4c43ac341368bcf9b8a03a57e96
Marc Gallardo <marc.gallardo.contractor@bbva.com>
2024-02-05 16:35:45 +0100

    set swift format

--- MarvelWikiApp/MarvelWikiAppTests/MarvelWikiAppTests.swift
+++ MarvelWikiApp/MarvelWikiAppTests/MarvelWikiAppTests.swift
@@ -5,11 +5,10 @@
 //  Created by Marc Gallardo Ruiz on 3/2/24.
 //
 
-import XCTest
 @testable import MarvelWikiApp
+import XCTest
 
 final class MarvelWikiAppTests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -28,9 +27,8 @@ final class MarvelWikiAppTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
--- MarvelWikiApp/MarvelWikiAppUITests/MarvelWikiAppUITestsLaunchTests.swift
+++ MarvelWikiApp/MarvelWikiAppUITests/MarvelWikiAppUITestsLaunchTests.swift
@@ -8,7 +8,6 @@
 import XCTest
 
 final class MarvelWikiAppUITestsLaunchTests: XCTestCase {
-
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
         true
     }
